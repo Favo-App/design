a8b27e75816b9cc675fb7c35e58ef9bc54ecdd1d
Edmo Lima <edmo.lima@aiyuapp.com.br>
2020-06-15 12:55:35 -0300

    add chromatic

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from './Button'
+
+export default {
+  title: 'Design System/components/Button',
+  component: Button,
+}
+
+export const ButtonExample = () => <Button />
