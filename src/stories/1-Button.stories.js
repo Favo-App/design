529de5c42d2dd5020007514b5aad285cbcc804a7
Edmo Lima <edmo.lima@aiyuapp.com.br>
2020-06-15 12:07:32 -0300

    add chromatic

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -12,7 +12,7 @@ export const Text = () => <Button onClick={action('clicked')}>Hello Button</Butt
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
     <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
+      😀 😎 👍 💯 hey
     </span>
   </Button>
 );
