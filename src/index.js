0bb3f5cd352beb68c0b781dcde5e8cb67d73a878
Edmo Lima <edmo.lima@aiyuapp.com.br>
2020-06-16 19:38:30 -0300

    fix(components): bug export component theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // exports standard material-ui components
 export { makeStyles } from '@material-ui/core/styles'
 
-//export theme
-export { theme } from './shared/themes'
+// export theme
+export { theme } from './shared/theme'
 
-//export components favoapp
+// export components favoapp
 export { default as Alert } from './components/Alert'
 export { default as Button } from './components/Button'
 export { default as TextField } from './components/TextField'
