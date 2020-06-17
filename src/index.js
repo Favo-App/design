f86a96fbdba795307ded16f15a3ec6291014fa7e
Edmo Lima <edmo.lima@aiyuapp.com.br>
2020-06-17 14:43:34 -0300

    feat(textfield): add export password file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ export { theme } from './shared/theme'
 // export components favoapp
 export { default as Alert } from './components/Alert'
 export { default as Button } from './components/Button'
-export { default as TextField } from './components/TextField'
-export { default as PasswordField } from './components/TextField'
+export { default as TextField } from './components/TextField/BaseTextField'
+export { default as PasswordField } from './components/TextField/PasswordField'
 export { default as Loader } from './components/Loader'
